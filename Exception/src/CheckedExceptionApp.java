c0f9b61af5f10e4b550ab6ed60f8cb555aa82364
jossi <rolety2202@naver.com>
2022-08-24 22:44:16 +0900

    finally 와 resource 다루기

--- Exception/src/CheckedExceptionApp.java
+++ Exception/src/CheckedExceptionApp.java
@@ -4,12 +4,22 @@ import java.io.IOException;
 public class CheckedExceptionApp {
 
 	public static void main(String[] args) {
+		FileWriter f=null;
 		try {
-			FileWriter f= new FileWriter("data.txt");
+			f= new FileWriter("data.txt");
 			f.write("Hello");
+			//close를 하기 전에 예외가 발생할 수 있기 떄문에 finally로 처리해야 합니다.
 			f.close();
 		}catch(IOException e) {
 			e.printStackTrace();
+		}finally {
+			if(f != null){
+			try {
+				f.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			}
 		}
 	}
 
