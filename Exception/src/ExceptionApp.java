52f838a83cbf391163b324a10dd88272ce8a8ede
jossi <rolety2202@naver.com>
2022-08-24 22:11:05 +0900

    우선순위

--- Exception/src/ExceptionApp.java
+++ Exception/src/ExceptionApp.java
@@ -6,14 +6,14 @@ public class ExceptionApp {
 		int[] scores = {10,20,30};
 		try {
 			System.out.println(2);
-			System.out.println(scores[3]);
+//			System.out.println(scores[3]);
 			System.out.println(3);
 			System.out.println(2/0);
 			System.out.println(4);
 		}catch(ArithmeticException e){
-			System.out.println("잘못된 계산입니다.");
-		}catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("없는 값을 찾고 계세요.");
+			System.out.println("계산이 잘못된 것 같아요.");
+		}catch(Exception e){
+			System.out.println("뭔가 이상합니다. 오류가 발생했습니다.");
 		}
 		System.out.println(5);
 	}
