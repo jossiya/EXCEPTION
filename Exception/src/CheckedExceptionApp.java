2ca09d658ccb2d5e40fead1b926ea6cef6aaa603
jossi <rolety2202@naver.com>
2022-08-24 22:26:29 +0900

    checked vs unchecked exception

---
+++ Exception/src/CheckedExceptionApp.java
@@ -0,0 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CheckedExceptionApp {
+
+	public static void main(String[] args) {
+		try {
+			FileWriter f= new FileWriter("data.txt");
+			f.write("Hello");
+			f.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
